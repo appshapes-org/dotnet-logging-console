867321b67109f1fae08b7daa32961e7d68634bf2
Rjae <rjae.easton@gmail.com>
2020-04-21 19:11:10 -0400

    Inline mutation testing commands

--- execute-mutation-tests.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-current_directory=$(pwd)
-pushd $(find . -name $1)
-dotnet tool restore; grep -oP -f $current_directory/stryker.project-reference.regex $1.csproj | xargs -I{} dotnet stryker --project-file={}
-popd
